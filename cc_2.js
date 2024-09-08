eafb43afca4a321a4ddbbf9dd9f2422c2ad824ce
k <kyle.offenbacher@gmail.com>
2024-09-08 14:56:09 -0400

    Implement and populate arrays for bills, tips, and totals

--- cc_2.js
+++ cc_2.js
@@ -21,3 +21,16 @@ console.log('The bill total with tip is:', bill + tip);
 //part 3
 
 //I already made a function for part 1, putting in this note to show I acknowledge part 3 but have already done it.
+
+//part 4
+
+let bills = [275, 40, 430];
+
+let tips = bills.map(bill => calculateTip(bill));
+
+let totals = bills.map((bill, index) => bill + tips[index]);
+
+console.log("Bills:",bills);
+console.log("Tips:", tips);
+console.log("Totals:", totals);
+
